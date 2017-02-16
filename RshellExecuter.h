7bcc813ff22e89527752b1cece80a9e2171f1ed5
nadnbuds <lalam003@ucr.edu>
2017-02-16 15:11:44 -0800

    Added (working?????) exe system

--- RshellExecuter.h
+++ RshellExecuter.h
@@ -1,9 +1,11 @@
-#include<iostream>
-#include<vector>
-#include<map>
+#include <iostream>
+#include <vector>
+#include <map>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 class RshellExecuter{
     
@@ -13,24 +15,30 @@ class RshellExecuter{
         bool RunCommand(vector<string> Command){
             pid_t pid;
             int status;
+            
+            //Convert Command into a char** args
+            char* p[Command.size()];
+            for(int x = 0; x < Command.size(); x ++){
+                p[x] = const_cast<char*>(Command.at(x).c_str());
+            }
+            char **args = p;
+            
+            //Running the Command
             pid = fork();
+            cout << pid;
             if(pid == -1){
                 perror("Fork");
                 exit(1);
                 return false;
             }
             else if(pid == 0){
-                if(execvp(Command.at(0).c_str(), Command) < 0){
+                if(execvp(args[0], args) < 0){
                     exit(1);
                     return false;
-                }                
+                }
             }
             else{
-                while(wait(&status) != pid)
-                {
-                    
-                }
-                return true;
+               while(wait(&status) != pid){}
                 
             }
         }
