033c078938d2b3a9bbd9fc0d2c660e5def5a58cb
nadnbuds <lalam003@ucr.edu>
2017-02-16 08:20:25 -0800

    Added proper Executer

--- RshellExecuter.h
+++ RshellExecuter.h
@@ -1,29 +1,38 @@
 #include<iostream>
 #include<vector>
 #include<map>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 class RshellExecuter{
-    enum BashCommands{
-        nodef,
-        close,
-    };
     
     public:
-        RshellExecuter(){
-            mapStringToBash["Exit"] = close;
-        }
+        RshellExecuter(){}
         
-        //
         bool RunCommand(vector<string> Command){
-            switch(mapStringToBash[Command.at(0)]){
-                    case close:
-                        
-                        break;
-                    default:
-                        break;
-                }
+            pid_t pid;
+            int status;
+            pid = fork();
+            if(pid == -1){
+                perror("Fork");
+                exit(1);
                 return false;
+            }
+            else if(pid == 0){
+                if(execvp(Command.at(0).c_str(), Command) < 0){
+                    exit(1);
+                    return false;
+                }                
+            }
+            else{
+                while(wait(&status) != pid)
+                {
+                    
+                }
+                return true;
+                
+            }
         }
     private:
-        map<string, BashCommands> mapStringToBash;
 };
