33b3d322f8570a77c094945980a30376d08f0807
nadnbuds <lalam003@ucr.edu>
2017-02-17 13:59:42 -0800

    Fixed Spacing bug

--- RshellReader.cpp
+++ RshellReader.cpp
@@ -16,13 +16,23 @@ void RshellReader::SpliceLine(){
 		if(Input.at(x) == spaceCheck){
 			string temp = Input.substr(0, x);
 			//Add a bound measure incase this goes over line.length() - 1
-			Input = Input.substr(x+1);
+			if(x + 1 < Input.length()){
+				Input = Input.substr(x+1);
+			}
+			else{
+				Input = "";
+			}
 			x = 0;
 			Commands.push_back(temp);
 		}
 		if(Input.at(x) == semiCheck){
 			string temp = Input.substr(0,x);
-			Input = Input.substr(x+1);
+			if(x + 1 < Input.length()){
+				Input = Input.substr(x+1);
+			}
+			else{
+				Input = "";
+			}
 			x = 0;
 			Commands.push_back(temp);
 			Commands.push_back(";");
